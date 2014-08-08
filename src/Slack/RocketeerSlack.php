f4d1a1a2c09cd493ca2484eff1fbe1645448f15d
Maxime Fabre <ehtnam6@gmail.com>
2014-08-08 14:02:19 +0200

    Update for 2.0 branch

--- src/Slack/RocketeerSlack.php
+++ src/Slack/RocketeerSlack.php
@@ -3,75 +3,76 @@ namespace Rocketeer\Plugins\Slack;
 
 use Crummy\Phlack\Phlack;
 use Illuminate\Container\Container;
+use Rocketeer\Plugins\AbstractNotifier;
 use Rocketeer\Plugins\Notifier;
 
-class RocketeerSlack extends Notifier
+class RocketeerSlack extends AbstractNotifier
 {
-  /**
-   * Setup the plugin
-   */
-  public function __construct(Container $app)
-  {
-    parent::__construct($app);
+	/**
+	 * Setup the plugin
+	 */
+	public function __construct(Container $app)
+	{
+		parent::__construct($app);
 
-    $this->configurationFolder = __DIR__.'/../config';
-  }
+		$this->configurationFolder = __DIR__.'/../config';
+	}
 
-  /**
-   * Bind additional classes to the Container
-   *
-   * @param Container $app
-   *
-   * @return void
-   */
-  public function register(Container $app)
-  {
-    $app->bind('slack', function ($app) {
-      return Phlack::factory(array(
-        'username' => $app['config']->get('rocketeer-slack::username'),
-        'token'    => $app['config']->get('rocketeer-slack::token'),
-      ));
-    });
+	/**
+	 * Bind additional classes to the Container
+	 *
+	 * @param Container $app
+	 *
+	 * @return void
+	 */
+	public function register(Container $app)
+	{
+		$app->bind('slack', function ($app) {
+			return Phlack::factory(array(
+				'username' => $app['config']->get('rocketeer-slack::username'),
+				'token'    => $app['config']->get('rocketeer-slack::token'),
+			));
+		});
 
-    return $app;
-  }
+		return $app;
+	}
 
-  /**
-   * Get the default message format
-   *
-   * @param string $message The message handle
-   *
-   * @return string
-   */
-  protected function getMessageFormat($message)
-  {
-    return $this->app['config']->get('rocketeer-slack::'.$message);
-  }
+	/**
+	 * Get the default message format
+	 *
+	 * @param string $message The message handle
+	 *
+	 * @return string
+	 */
+	protected function getMessageFormat($message)
+	{
+		return $this->app['config']->get('rocketeer-slack::'.$message);
+	}
 
-  /**
-   * Send a given message
-   *
-   * @param string $message
-   *
-   * @return void
-   */
-  public function send($message)
-  {
-    $messageBuilder = $this->slack->getMessageBuilder();
-    $room    = $this->config->get('rocketeer-slack::room');
+	/**
+	 * Send a given message
+	 *
+	 * @param string $message
+	 *
+	 * @return void
+	 */
+	public function send($message)
+	{
+		$messageBuilder = $this->slack->getMessageBuilder();
+		$room           = $this->config->get('rocketeer-slack::room');
 
-    // Build base message
-    $messageBuilder
-      ->setText($message)
-      ->setChannel($room);
+		// Build base message
+		$messageBuilder
+			->setText($message)
+			->setChannel($room);
 
-    // Add optional emoji
-    if ($emoji = $this->config->get('rocketeer-slack::emoji')) {
-      $messageBuilder->setIconEmoji($emoji);
-    }
+		// Add optional emoji
+		if ($emoji = $this->config->get('rocketeer-slack::emoji')) {
+			$messageBuilder->setIconEmoji($emoji);
+		}
 
-    $response = $this->slack->send($messageBuilder->create());
+		$response = $this->slack->send($messageBuilder->create());
 
-    return $response;
-  }
+		return $response;
+	}
 }

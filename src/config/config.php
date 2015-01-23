470d4356c73cf2d178ff8baaf8873634d366dd58
Maxime Fabre <ehtnam6@gmail.com>
2015-01-23 14:22:12 +0100

    Add message for rollback event

--- src/config/config.php
+++ src/config/config.php
@@ -13,8 +13,9 @@ return array(
 	// 2: Branch
 	// 3: Connection and stage
 	// 4: Host
-	'before_deploy' => '{1} is deploying "{2}" on "{3}" ({4})',
-	'after_deploy'  => '{1} finished deploying branch "{2}" on "{3}" ({4})',
+	'before_deploy'  => '{1} is deploying "{2}" on "{3}" ({4})',
+	'after_deploy'   => '{1} finished deploying branch "{2}" on "{3}" ({4})',
+	'after_rollback' => '{1} rolled back branch "{2}" on "{3}" to previous version ({4})',
 
 	// Default emoji to use as the bot's avatar
 	'emoji'   => 'rocket',

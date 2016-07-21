522a42b9ba361d074f7a3bad26f79c469c108239
Maxime Fabre <ehtnam6@gmail.com>
2016-07-21 12:19:21 +0200

    Add halt message

--- src/config/config.php
+++ src/config/config.php
@@ -15,6 +15,7 @@ return [
     // 4: Host
     'before_deploy' => '{1} is deploying "{2}" on "{3}" ({4})',
     'after_deploy' => '{1} finished deploying branch "{2}" on "{3}" ({4})',
+    'halt_deploy' => 'Failed to deploy branch {2} on "{3}" ({4})',
     'after_rollback' => '{1} rolled back branch "{2}" on "{3}" to previous version ({4})',
 
     // Default emoji to use as the bot's avatar
